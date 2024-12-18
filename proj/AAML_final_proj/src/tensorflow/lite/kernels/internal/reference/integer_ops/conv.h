c0d1381ef2eca60652c07317fd26dc3260cb05f6
weiluohuang <huang.cs13@nycu.edu.tw>
2024-12-18 13:52:45 +0800

    improve conv.h, 8749K

--- proj/AAML_final_proj/src/tensorflow/lite/kernels/internal/reference/integer_ops/conv.h
+++ proj/AAML_final_proj/src/tensorflow/lite/kernels/internal/reference/integer_ops/conv.h
@@ -51,59 +51,57 @@ inline void ConvPerChannel(
   const int patch_size = filter_height * filter_width * filter_input_depth;
   const int patch_num = output_height * output_width;
   const int32_t input_offset = params.input_offset;
-  int8_t kernel[324*36];
-  int8_t im2col[1024*324];
-  std::fill_n(im2col, patch_num * patch_size, -input_offset);
   for (int out_y = 0; out_y < output_height; ++out_y) {
     const int in_y_origin = (out_y * stride_height) - pad_height;
-    for (int out_x = 0; out_x < output_width; ++out_x) {
+    for (int out_x = 0; out_x < output_width; out_x+=4) {
       const int in_x_origin = (out_x * stride_width) - pad_width;
       for (int filter_y = 0; filter_y < filter_height; ++filter_y) {
         const int in_y = in_y_origin + dilation_height_factor * filter_y;
         for (int filter_x = 0; filter_x < filter_width; ++filter_x) {
           const int in_x = in_x_origin + dilation_width_factor * filter_x;
 
-          // Zero padding by omitting the areas outside the image.
-          const bool is_point_inside_image =
-            (in_x >= 0) && (in_x < input_width) && (in_y >= 0) && (in_y < input_height);
+          int i = (out_y * output_width + out_x);
 
-          if (!is_point_inside_image) continue;
-
-          for (int in_channel = 0; in_channel < filter_input_depth; ++in_channel) {
-            im2col[(out_y * output_width + out_x) * patch_size +
-                (filter_y * filter_width * filter_input_depth +
-                 filter_x * filter_input_depth + in_channel)] =
-                input_data[Offset(input_shape, 0, in_y, in_x, in_channel)];
+          // Zero padding by omitting the areas outside the image (for in_y).
+          if (__builtin_expect((uint32_t)in_y >= (uint32_t)input_height, 0)) {
+            int8_t n = -input_offset;
+            int j = (filter_y * filter_width * filter_input_depth + filter_x * filter_input_depth);
+            for (int in_channel = 0; in_channel < filter_input_depth; ++in_channel, ++j) {
+              cfu_op0(0, ((uint32_t)(uint8_t)n<<24) | ((uint32_t)(uint8_t)n<<16) | 
+              ((uint32_t)(uint8_t)n<<8 ) | (uint32_t)(uint8_t)n, (i>>2)*patch_size+j);
+            }
+          } else {
+            int8_t n = -input_offset;
+            int j = (filter_y * filter_width * filter_input_depth + filter_x * filter_input_depth);
+            for (int in_channel = 0; in_channel < filter_input_depth; ++in_channel, ++j) {
+              int8_t n1 = ((uint32_t)in_x >= (uint32_t)input_width) ? n : input_data[Offset(input_shape, 0, in_y, in_x, in_channel)];
+              int8_t n2 = ((uint32_t)(in_x + stride_width) >= (uint32_t)input_width) ? n : input_data[Offset(input_shape, 0, in_y, in_x + stride_width, in_channel)];
+              int8_t n3 = ((uint32_t)(in_x + stride_width*2) >= (uint32_t)input_width) ? n : input_data[Offset(input_shape, 0, in_y, in_x + 2*stride_width, in_channel)];
+              int8_t n4 = ((uint32_t)(in_x + stride_width*3) >= (uint32_t)input_width) ? n : input_data[Offset(input_shape, 0, in_y, in_x + 3*stride_width, in_channel)];
+              cfu_op0(0, ((uint32_t)(uint8_t)n1<<24) | ((uint32_t)(uint8_t)n2<<16) | ((uint32_t)(uint8_t)n3<<8) | (uint32_t)(uint8_t)n4, (i>>2)*patch_size+j);
+            }
           }
         }
       }
     }
   }
  
-  for (int out_channel = 0; out_channel < output_depth; ++out_channel) {
+  for (int out_channel = 0; out_channel < output_depth; out_channel+=4) {
     for (int filter_y = 0; filter_y < filter_height; ++filter_y) {
       for (int filter_x = 0; filter_x < filter_width; ++filter_x) {
-        for (int in_channel = 0; in_channel < filter_input_depth; ++in_channel) {
-          kernel[((filter_y * filter_width * filter_input_depth + filter_x * filter_input_depth + in_channel) * output_depth)
-          +out_channel] = filter_data[Offset(filter_shape, out_channel, filter_y, filter_x, in_channel)];
+        int i = (filter_y * filter_width * filter_input_depth + filter_x * filter_input_depth);
+        for (int in_channel = 0; in_channel < filter_input_depth; ++in_channel, ++i) {
+          int8_t n1 = filter_data[Offset(filter_shape, out_channel, filter_y, filter_x, in_channel)];
+          int8_t n2 = filter_data[Offset(filter_shape, out_channel+1, filter_y, filter_x, in_channel)];
+          int8_t n3 = filter_data[Offset(filter_shape, out_channel+2, filter_y, filter_x, in_channel)];
+          int8_t n4 = filter_data[Offset(filter_shape, out_channel+3, filter_y, filter_x, in_channel)];
+          cfu_op0(1, ((uint32_t)(uint8_t)n1<<24) | ((uint32_t)(uint8_t)n2<<16) | ((uint32_t)(uint8_t)n3<<8) | (uint32_t)(uint8_t)n4, (out_channel>>2)*patch_size+i);
         }
       }
     }
   }
 
   const int32_t KMN = ((int32_t)patch_size << 21) | ((int32_t)patch_num << 9) | (int32_t)output_depth;
-  for (int i = 0; i < patch_num; i += 4) {
-    for (int j = 0; j < patch_size; ++j) {
-      int t = i*patch_size+j;
-       cfu_op0(0, ((uint32_t)(uint8_t)im2col[t]<<24) | ((uint32_t)(uint8_t)im2col[t+patch_size]<<16) | ((uint32_t)(uint8_t)im2col[t+patch_size+patch_size]<<8) | (uint32_t)(uint8_t)im2col[t+patch_size+patch_size+patch_size], (i>>2)*patch_size+j);
-    }
-  }
-  for (int j = 0; j < output_depth; j += 4) {
-    for (int i = 0; i < patch_size; ++i) {
-      int t = i*output_depth+j;
-      cfu_op0(1, ((uint32_t)(uint8_t)kernel[t]<<24) | ((uint32_t)(uint8_t)kernel[t+1]<<16) | ((uint32_t)(uint8_t)kernel[t+2]<<8) | (uint32_t)(uint8_t)kernel[t+3], (j>>2)*patch_size+i);
-    }
-  }
   cfu_op0(3, KMN, input_offset);
 
   for (int j = 0; j < output_depth; ++j){
