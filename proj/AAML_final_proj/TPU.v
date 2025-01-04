b0a3c28362624f86d9f21ee31d7120d0bf7142e9
weiluohuang <huang.cs13@nycu.edu.tw>
2025-01-04 23:15:28 +0800

    add tranning script, update add.h, README, and TPU

--- proj/AAML_final_proj/TPU.v
+++ proj/AAML_final_proj/TPU.v
@@ -121,7 +121,7 @@ always @(posedge clk) begin
         M_reg <= M;
         N_reg <= N;
         input_offset_reg <= input_offset;
-        for (i = 0; i < 32'd32; i = i + 1)
+        for (i = 0; i < 32'd16; i = i + 1)
             PE[i] <= 32'b0;
         A_Block_index <= 15'b0;
         B_Block_index <= 15'b0;
@@ -136,31 +136,17 @@ always @(posedge clk) begin
         A_index_reg <= counter + A_Block_index * K_reg;
         B_index_reg <= counter + B_Block_index * K_reg;
         end
-        if(counter > 2'd2 && counter <= K_reg + 2'd2)begin
-            PE[ 0] <= PE[ 0] + prod[ 0];
-            PE[ 1] <= PE[ 1] + prod[ 1];
-            PE[ 2] <= PE[ 2] + prod[ 2];
-            PE[ 3] <= PE[ 3] + prod[ 3];
-            PE[ 4] <= PE[ 4] + prod[ 4];
-            PE[ 5] <= PE[ 5] + prod[ 5];
-            PE[ 6] <= PE[ 6] + prod[ 6];
-            PE[ 7] <= PE[ 7] + prod[ 7];
-            PE[ 8] <= PE[ 8] + prod[ 8];
-            PE[ 9] <= PE[ 9] + prod[ 9];
-            PE[10] <= PE[10] + prod[10];
-            PE[11] <= PE[11] + prod[11];
-            PE[12] <= PE[12] + prod[12];
-            PE[13] <= PE[13] + prod[13];
-            PE[14] <= PE[14] + prod[14];
-            PE[15] <= PE[15] + prod[15];
-        end else if(counter >= K_reg + 3'd3 && counter <= K_reg + 3'd6)begin
-            C_wr_en_reg <= 1;
-            C_index_reg <= counter - K_reg - 3'd3 + A_Block_index * 3'd4 + B_Block_index * M_reg;
+        if(counter > 2'd2 && counter <= K_reg + 2'd2)
+            for (i = 0; i < 32'd16; i = i + 1)
+                PE[i] <= PE[i] + prod[i];
+        else if(counter >= K_reg + 3'd3 && counter <= K_reg + 3'd6)begin
+            C_wr_en_reg   <= 1;
+            C_index_reg   <= counter - K_reg - 3'd3 + A_Block_index * 3'd4 + B_Block_index * M_reg;
             C_data_in_reg <= (counter - K_reg == 16'd3)? {PE[ 0], PE[ 1], PE[ 2], PE[ 3]}
-                        :  (counter - K_reg == 16'd4)? {PE[ 4], PE[ 5], PE[ 6], PE[ 7]}
-                        :  (counter - K_reg == 16'd5)? {PE[ 8], PE[ 9], PE[10], PE[11]}
-                        :  (counter - K_reg == 16'd6)? {PE[12], PE[13], PE[14], PE[15]}
-                        :  128'bx;
+                          :  (counter - K_reg == 16'd4)? {PE[ 4], PE[ 5], PE[ 6], PE[ 7]}
+                          :  (counter - K_reg == 16'd5)? {PE[ 8], PE[ 9], PE[10], PE[11]}
+                          :  (counter - K_reg == 16'd6)? {PE[12], PE[13], PE[14], PE[15]}
+                          :  128'bx;
         end else if(counter == K_reg + 4'd7 || (counter == 16'd7 && K_reg == 8'd2))begin
             if(A_Block_index == A_Block_num && B_Block_index == B_Block_num)begin
                 done <= 1;
@@ -171,7 +157,7 @@ always @(posedge clk) begin
             end else
                 A_Block_index <= A_Block_index + 1;
             counter <= 16'b0;
-            for (i = 0; i < 32'd32; i = i + 1)
+            for (i = 0; i < 32'd16; i = i + 1)
                 PE[i] <= 32'b0;
         end
     end
